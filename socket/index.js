a0935e077764a93e256ea983562af452519f7d6d
satyam pal <satyam.pal@planify.in>
2024-10-03 15:45:16 +0530

    change clint url

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,6 @@
 const { Server } = require("socket.io");
 
-const io = new Server({cors:"http://localhost:5173" });
+const io = new Server({cors:"https://chat-app-ten-psi-94.vercel.app/" });
 
 let onlineUsers=[]
 
